efa53d2933185938bb7709c23376579730d66b1a
Daniel Velez <velezdaniel516@gmail.com>
2025-03-18 09:07:17 -0400

    add container class and import ListForm component

---
+++ src/components/TaskifyList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ListForm from './ListForm'
+
+const TaskifyList = () => {
+    return (
+        <div className='container bg-blue-950 mt-20 p-8
+        rounded-md'>
+            to do list
+
+            <ListForm/>
+        </div>
+    )
+} 
+
+export default TaskifyList
