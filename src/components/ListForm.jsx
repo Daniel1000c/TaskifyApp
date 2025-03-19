d3c41b4d9452518076759d3ecd6ee934d94cb3b2
Daniel Velez <velezdaniel516@gmail.com>
2025-03-19 12:04:47 -0400

    add prevent default submit function and add collect user input function

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ListForm = () => {
+const ListForm = ({createTodo}) => {
+    const [value, setValue] = useState('')
+
+    {/*Create form submit function */}
+    const formSubmit = e => {
+        //Prevent form default form submission
+        e.preventDefault()
+
+        createTodo(value)
+
+        setValue('')
+    }
+
     return (
-        <form className='mb-4  font-primary w-full'>
-            <input type="text" className='outline-none bg-transparent border border-gray-500 p-4 w-[300px]
-            text-white mb-8 rounded placeholder:text-gray-300'
-            placeholder='What task do you want to add?'/>
+        <form className='mb-4  font-primary w-full' onSubmit={formSubmit}>
 
-            <button className='bg-gray-700 border-none p-2
-            text-white cursor-pointer rounded ml-2'>Add</button>
+            {/*Add text form input */}
+            <input type="text" className='outline-none bg-transparent border border-white-500 p-4 w-[300px]
+            text-white mb-8 rounded placeholder:text-gray-300'
+            placeholder='What task do you want to add?' onChange={(e) => setValue(e.target.value)} value={value} name='task' id='task'/>
+
+
+            {/*Create add button */}
+            <button className='bg-[#D38027] inline-block border-none font-bold font-mono text-lg tracking-wide text-white cursor-pointer rounded-full ml-2 w-[100px] p-3'>Add</button>
         </form>
     )
 }
