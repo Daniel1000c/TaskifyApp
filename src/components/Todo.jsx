ebdd739a60c3f42ad29b797abc86ca89f52c4ae7
Daniel Velez <velezdaniel516@gmail.com>
2025-03-19 12:05:26 -0400

    add to do list item container and css formatting

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,17 @@
 import React from "react"
+import {AiFillEdit} from "react-icons/ai" //Import edit icon
+import {BsFillTrashFill} from "react-icons/bs" //Import Trash Icon
 
-const Todo = () => {
+const Todo = ({task}) => {
     return (
-        <div>to do</div>
+        <div className="flex justify-between items-center bg-[#1b263b] text-white py-3 px-4 rounded-md
+        mb-1 cursor-pointer">
+            <p className="font-serif text-lg">{task.task}</p>
+            <div className="flex items-center gap-x-4">
+                <AiFillEdit className="text-xl"/>
+                <BsFillTrashFill className="text-xl"/>
+            </div>
+        </div>
     )
 }
 
