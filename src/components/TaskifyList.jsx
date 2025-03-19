c70d4a2f3aec2d599e29f993ea2f3e62535e4fb9
Daniel Velez <velezdaniel516@gmail.com>
2025-03-19 12:06:19 -0400

    add todo component and change background color of taskify container

--- src/components/TaskifyList.jsx
+++ src/components/TaskifyList.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React , { useState } from 'react'
 import ListForm from './ListForm'
+import { v4 as uuidv4 } from 'uuid' //Import uuid identifier to create unique ids for each task
+import Todo from './Todo'
+uuidv4()
 
 const TaskifyList = () => {
-    return (
-        <div className='container bg-blue-950 mt-20 p-8
-        rounded-md'>
-            to do list
 
-            <ListForm/>
+    {/*Add use state*/}
+    const [toDoValue, setTodo] = useState([])
+
+    {/*Create "createTodo" function*/}
+    const createTodo = todo => {
+        setTodo([...toDoValue, {id: uuidv4(), task: todo, isEditing: false}])
+    }
+    return (
+        <div className='container bg-[#266dd3] mt-20 p-8 rounded-md'>
+            
+            {/*Add taskify title */}
+            <h1 className='text-white font-bold font-mono tracking-wider text-4xl mb-4'>Taskify</h1>
+
+            {/*Add list form input component */}
+            <ListForm createTodo = {createTodo}/>
+
+            {/*Add todo map*/}
+            {toDoValue.map((todo, index) => (
+                <Todo task={todo} key={index} />
+            ))}
         </div>
     )
 } 
