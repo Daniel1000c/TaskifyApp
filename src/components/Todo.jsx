098995aec162fd469bbdaa814001f4a9c82e93ea
Daniel Velez <velezdaniel516@gmail.com>
2025-03-20 09:19:38 -0400

    create edit task and delete task functions

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,14 +2,17 @@ import React from "react"
 import {AiFillEdit} from "react-icons/ai" //Import edit icon
 import {BsFillTrashFill} from "react-icons/bs" //Import Trash Icon
 
-const Todo = ({task}) => {
+const Todo = ({task, deleteTask, editTask}) => {
     return (
         <div className="flex justify-between items-center bg-[#1b263b] text-white py-3 px-4 rounded-md
         mb-1 cursor-pointer">
             <p className="font-serif text-lg">{task.task}</p>
             <div className="flex items-center gap-x-4">
-                <AiFillEdit className="text-xl"/>
-                <BsFillTrashFill className="text-xl"/>
+                {/*Edit Icon */}
+                <AiFillEdit className="text-xl" onClick={() => editTask(task.id)}/>
+
+                {/*Delete Icon */}  
+                <BsFillTrashFill className="text-xl" onClick={() => deleteTask(task.id)}/>
             </div>
         </div>
     )
