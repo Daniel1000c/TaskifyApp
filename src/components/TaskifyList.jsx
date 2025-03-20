7a5ff7cdba32e712584e0452b8fac9cf3f1e93ae
Daniel Velez <velezdaniel516@gmail.com>
2025-03-20 09:20:16 -0400

    add delete task and edit task toggle functions"

--- src/components/TaskifyList.jsx
+++ src/components/TaskifyList.jsx
@@ -2,6 +2,7 @@ import React , { useState } from 'react'
 import ListForm from './ListForm'
 import { v4 as uuidv4 } from 'uuid' //Import uuid identifier to create unique ids for each task
 import Todo from './Todo'
+import EditTask from './EditTask'
 uuidv4()
 
 const TaskifyList = () => {
@@ -13,8 +14,33 @@ const TaskifyList = () => {
     const createTodo = todo => {
         setTodo([...toDoValue, {id: uuidv4(), task: todo, isEditing: false}])
     }
+    
+    {/*Create "deleteTask" function */}
+    const deleteTask = id => {
+        setTodo(toDoValue.filter(todo => todo.id !== id))
+    }
+
+    {/*Create "editTask" function */}
+    const editTask = id => {
+        setTodo(
+            toDoValue.map((todo) =>
+                todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo
+            )
+        )
+    }
+
+    {/*Create  update "editTodo" function */}
+    const editTodo = (task, id) => {
+       setTodo(
+        toDoValue.map((todo) =>
+            todo.id === id ? {...todo, task, isEditing: false} : todo
+        )
+       )
+    }
+    
     return (
-        <div className='container bg-[#266dd3] mt-20 p-8 rounded-md'>
+        
+        <div className=' bg-[#266dd3] mt-20 p-8 rounded-md'>
             
             {/*Add taskify title */}
             <h1 className='text-white font-bold font-mono tracking-wider text-4xl mb-4'>Taskify</h1>
@@ -24,7 +50,11 @@ const TaskifyList = () => {
 
             {/*Add todo map*/}
             {toDoValue.map((todo, index) => (
-                <Todo task={todo} key={index} />
+                todo.isEditing ? (
+                    <EditTask editTodo={editTodo} task={todo} key={index}/>
+                ) : (
+                    <Todo task={todo} key={index} deleteTask={deleteTask} editTask={editTask}/>
+                )
             ))}
         </div>
     )
